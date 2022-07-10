b0350eac726e289a7b2cbd970633ff7dfcb00d08
ohtaekwon <ohtk1119@gmail.com>
2022-07-10 23:03:21 +0900

    feat:초기세팅설정 수정

---
+++ src/Router.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Main from "./pages/main";
+import MyList from "./pages/myList";
+
+export default function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />}></Route>
+      <Route path="/my-list" element={<MyList />}></Route>
+      <Route path="/*" element={<div>404 오류 page가 없습니다.</div>}></Route>
+    </Routes>
+  );
+}
---
+++ src/pages/myList/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MyList() {
+  return <div>MyList페이지</div>;
+}
+
+export default MyList;
